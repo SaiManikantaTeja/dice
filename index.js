72ce1548fc6229ad165f4e2ff089a893bc6e4d93
SaiManikantaTeja <saimani.bongurala@gmail.com>
2023-01-13 16:36:34 +0530

    added flag icon

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 // Result 
 if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 wins!";
-}else if (randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 2 wins!";
-}else {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
+}
+else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+}
+else {
     document.querySelector("h1").innerHTML ="Draw!";
 }
